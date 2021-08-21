b6186d93cee7e22ce133f4ed12751649e2f1736f
bartbzd <bbart318@gmail.com>
2021-08-20 22:52:20 -0500

    Pass all tests - repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let repeatedString = "";
+  for (i = 0; i < number; i++) {
+    repeatedString += string;
+  }
+  if (number < 0) {
+    return "ERROR";
+  }
+  return repeatedString;
 };
 
 module.exports = repeatString;

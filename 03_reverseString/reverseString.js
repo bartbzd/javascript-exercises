65c6f8a46c4beae7d743ac07293bae4ae3447e9b
bartbzd <bbart318@gmail.com>
2021-08-22 00:09:18 -0500

    Pass all reverseString Jest tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function(str) {
-
+const reverseString = function (str) {
+  let splitStr = str.split("");
+  let reverseArray = splitStr.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
 };
 
 module.exports = reverseString;

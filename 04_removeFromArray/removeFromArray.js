780a7f2df56eabb6634ddccd0bbccc26828e6e8d
bartbzd <bbart318@gmail.com>
2021-08-23 22:50:10 -0500

    Pass first test and work on second test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...n) {
+  return n.map((element) => {
+    return array.splice(n, 1);
+  });
+};
 
+const removeFromArray2 = function (array, n) {
+  array.splice(n - 1, 1);
+  return array;
 };
 
 module.exports = removeFromArray;

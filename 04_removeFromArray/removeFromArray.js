5b21aa3c0bbd5e4a32de553f338a3a79f3473eef
bartbzd <bbart318@gmail.com>
2021-08-24 23:33:03 -0500

    Pass all Jest tests for removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,32 @@
-const removeFromArray = function (array, ...n) {
-  return n.map((element) => {
-    return array.splice(n, 1);
-  });
+//declare function with arguments
+const removeFromArray = function (arr, ...args) {
+  //create variable to capture ...args
+  let n;
+  //create new array to not change original
+  let newArr = arr.slice();
+  //loop / increment through array length
+  for (let i = 0; i < arr.length; i++) {
+    //store returned ...arg index in n
+    n = newArr.indexOf(args[i]);
+    //ensure n exists and if it does, run code
+    if (n > -1) {
+      //splice out 1 element from new array, starting at n
+      newArr.splice(n, 1);
+    }
+  }
+  //return new array with removed elements
+  return newArr;
 };
 
-const removeFromArray2 = function (array, n) {
-  array.splice(n - 1, 1);
-  return array;
-};
+//Example solution
+// const removeFromArray = function (array, ...toRemove) {
+//   let modifiedArray = array.slice();
 
+//   for (let i = 0; i < array.length; i++) {
+//     if (modifiedArray.includes(toRemove[i])) {
+//       modifiedArray.splice(modifiedArray.indexOf(toRemove[i]), 1);
+//     }
+//   }
+//   return modifiedArray;
+// };
 module.exports = removeFromArray;

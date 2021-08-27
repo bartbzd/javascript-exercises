abc788d532c79b065591273a7ed0d23c295f5694
bartbzd <bbart318@gmail.com>
2021-08-26 23:17:17 -0500

    Refactor solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,12 @@
 const reverseString = function (str) {
-  let splitStr = str.split("");
-  let reverseArray = splitStr.reverse();
-  let joinArray = reverseArray.join("");
-  return joinArray;
-};
+  let splitStr = str.split("")
+  let reverseArray = splitStr.reverse()
+  let joinArray = reverseArray.join("")
+  return joinArray
+}
 
 const reverseString2 = function (str) {
-  return str.split("").reverse().join("");
-};
+  return str.split("").reverse().join("")
+}
 
-module.exports = reverseString;
+module.exports = reverseString

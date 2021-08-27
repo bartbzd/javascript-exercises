4c5a1c113849f96ce5fd9e7cc7227cbdd56cf5e0
bartbzd <bbart318@gmail.com>
2021-08-27 00:13:10 -0500

    Refactor code and add example solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,34 +1,34 @@
 const sumAll = function (num1, num2) {
   if (num1 < 0 || num2 < 0 || isNaN(num1) || isNaN(num2)) {
-    return "ERROR";
+    return "ERROR"
   }
   if (num1 > num2) {
     //rearranage num1 and num2 (tip from solution)
-    const temp = num1;
-    num1 = num2;
-    num2 = temp;
+    const temp = num1
+    num1 = num2
+    num2 = temp
   }
-  let sum = 0;
+  let sum = 0
   for (let i = num1; i < num2 + 1; i++) {
-    sum += i;
+    sum += i
   }
-  return sum;
-};
+  return sum
+}
 
 // Example solution
 const sumAll = function (min, max) {
-  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
-  if (min < 0 || max < 0) return "ERROR";
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"
+  if (min < 0 || max < 0) return "ERROR"
   if (min > max) {
-    const temp = min;
-    min = max;
-    max = temp;
+    const temp = min
+    min = max
+    max = temp
   }
-  let sum = 0;
+  let sum = 0
   for (let i = min; i < max + 1; i++) {
-    sum += i;
+    sum += i
   }
-  return sum;
-};
+  return sum
+}
 
-module.exports = sumAll;
+module.exports = sumAll

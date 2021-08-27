75296612fa33a853dfe666b13580e0be81d4a150
bartbzd <bbart318@gmail.com>
2021-08-26 22:25:30 -0500

    Create and finish sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+let finalSum = 0;
+let bigNum;
+let smallNum;
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0 || isNaN(num1) || isNaN(num2)) {
+    return "ERROR";
+  } else if (num1 > num2) {
+    bigNum = num1;
+    smallNum = num2;
+  } else {
+    bigNum = num2;
+    smallNum = num1;
+  }
+  for (let i = 0; i < bigNum; i++) {
+    finalSum += smallNum;
+    smallNum++;
+  }
+  let sumValue = finalSum;
+  finalSum = 0;
+  return sumValue;
 };
 
 module.exports = sumAll;

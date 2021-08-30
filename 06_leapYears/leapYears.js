5cb436e60c3dd72f54cfbcc24e30a3320a902487
bartbzd <bbart318@gmail.com>
2021-08-29 22:43:31 -0500

    Write leapYear function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (year % 100 == 0 && !(year % 400 == 0)) {
+    return false
+  } else if (year % 4 == 0 || year % 400 == 0) {
+    return true
+  } else return false
+}
 
-};
+module.exports = leapYears
 
-module.exports = leapYears;
+// - divisible by 4 or 400 but not 100
+// - not divisible by 100 unless 400

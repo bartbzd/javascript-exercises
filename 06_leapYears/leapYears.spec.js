909e7891f00f5318217082b4f407b30253f0a56a
bartbzd <bbart318@gmail.com>
2021-08-29 22:43:21 -0500

    Revise test wording

--- 06_leapYears/leapYears.spec.js
+++ 06_leapYears/leapYears.spec.js
@@ -1,22 +1,22 @@
-const leapYears = require('./leapYears')
+const leapYears = require("./leapYears")
 
-describe('leapYears', () => {
-  test('works with non century years', () => {
-    expect(leapYears(1996)).toBe(true);
-  });
-  test.skip('works with non century years', () => {
-    expect(leapYears(1997)).toBe(false);
-  });
-  test.skip('works with ridiculously futuristic non century years', () => {
-    expect(leapYears(34992)).toBe(true);
-  });
-  test.skip('works with century years', () => {
-    expect(leapYears(1900)).toBe(false);
-  });
-  test.skip('works with century years', () => {
-    expect(leapYears(1600)).toBe(true);
-  });
-  test.skip('works with century years', () => {
-    expect(leapYears(700)).toBe(false);
-  });
-});
+describe("leapYears", () => {
+  test("works with non century years, 1996 to be true", () => {
+    expect(leapYears(1996)).toBe(true)
+  })
+  test("works with non century years, 1997 to be false", () => {
+    expect(leapYears(1997)).toBe(false)
+  })
+  test("works with ridiculous years, 34992 to be true", () => {
+    expect(leapYears(34992)).toBe(true)
+  })
+  test("works with century years, 1900 to false", () => {
+    expect(leapYears(1900)).toBe(false)
+  })
+  test("works with century years, 1600 to be true", () => {
+    expect(leapYears(1600)).toBe(true)
+  })
+  test("works with century years, 700 to be false", () => {
+    expect(leapYears(700)).toBe(false)
+  })
+})

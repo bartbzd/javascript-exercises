cd32201e8bd5917462595ccdfe9bf21223d7b438
bartbzd <bbart318@gmail.com>
2021-08-29 23:57:45 -0500

    Add solution and test around

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -8,5 +8,13 @@ const leapYears = function (year) {
 
 module.exports = leapYears
 
-// - divisible by 4 or 400 but not 100
-// - not divisible by 100 unless 400
+// Shorter example solution
+// const leapYears = function(year) {
+//   return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0)
+// };
+
+//divisible by 4 and 400 but not 100
+// return year % 4 === 0 AND (year % 100 !== 0 OR year % 400 === 0)
+// 2000          TRUE                 FALSE                TRUE     = TRUE
+// 1985          FALSE                TRUE                 FALSE    = FALSE
+//34992         TRUE                 TRUE                FALSE     = TRUE

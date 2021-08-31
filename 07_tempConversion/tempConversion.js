bbf17f998a18962e4769b6194268d299abdb5f60
bartbzd <bbart318@gmail.com>
2021-08-30 22:50:49 -0500

    Add solution and rework code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,16 +1,21 @@
 const ftoc = function (f) {
-  let c = 0
-  c = (f - 32) * (5 / 9)
-  return Math.round(c * 10) / 10
+  return Math.round((f - 32) * (5 / 9) * 10) / 10
 }
 
 const ctof = function (c) {
-  let f = 0
-  f = c * (9 / 5) + 32
-  return Math.round(f * 10) / 10
+  return Math.round(((c * 9) / 5 + 32) * 10) / 10
 }
 
 module.exports = {
   ftoc,
   ctof,
 }
+
+//Example solutions
+// const ftoc = function(f) {
+//   return Math.round((f - 32) * (5/9) * 10) / 10
+//  };
+
+//  const ctof = function(c) {
+//    return Math.round(((c * 9/5) + 32) * 10) / 10
+//  };

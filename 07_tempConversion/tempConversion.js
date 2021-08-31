62725640aff6d4b1ae3f84321c534a4e79980f49
bartbzd <bbart318@gmail.com>
2021-08-30 22:44:22 -0500

    Write functions that convert between Celsius and Fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,16 @@
-const ftoc = function() {
+const ftoc = function (ftemp) {
+  let ctemp = 0
+  ctemp = (ftemp - 32) * (5 / 9)
+  return Math.round(ctemp * 10) / 10
+}
 
-};
-
-const ctof = function() {
-
-};
+const ctof = function (ctemp) {
+  let ftemp = 0
+  ftemp = ctemp * (9 / 5) + 32
+  return Math.round(ftemp * 10) / 10
+}
 
 module.exports = {
   ftoc,
-  ctof
-};
+  ctof,
+}

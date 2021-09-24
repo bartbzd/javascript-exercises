8b20447cb27a4b2f1fa89d4baf6c20c07b264c92
Bart PC <bbart318@gmail.com>
2021-09-23 22:03:35 -0500

    Completed palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = (words) => {
+  let newWord = words.replace(/\W/g, "").toLowerCase();
+  let word = newWord.split("");
+  let reversed = word.reverse().join("");
+  return newWord === reversed;
 };
 
 module.exports = palindromes;
+
+//remove non-alphanumeric and change to lower case
+//split string into array
+//reverse array and join back into string
+
+//'A movie!' ---> 'amovie' = newWord
+//'a, m, o, v, i, e'
+//'e, i, v, o, m, a' ---> 'eivoma' = reversed
